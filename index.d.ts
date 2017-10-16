dc085fde15a28966c83372a81e61bee87b45428e
Vincent Pang <augostino@gmail.com>
2017-10-17 03:34:52 +0800

    add typing file for typescript

---
+++ index.d.ts
@@ -0,0 +1,23 @@
+declare namespace CanCan {
+    export interface Option {
+        instanceOf: <T, U>(instance: T, model: U) => boolean;
+        createError: () => any;
+    }
+}
+
+declare class CanCan {
+    public constructor(options?: CanCan.Option);
+
+    public allow<T, U>(model: T,
+                       actions: string | string[],
+                       targets: U | U[] | string | string[],
+                       condition?: any | (<T, U>(performer: T, target: U) => boolean)): void;
+
+    public can<T, U>(performer: T, action: string, target: U): boolean;
+
+    public cannot<T, U>(performer: T, action: string, target: U): boolean;
+
+    public authorize<T, U>(performer: T, action: string, target: U): void;
+}
+
+export = CanCan;
