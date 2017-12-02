a2a21e0c1aec67d3826dd96443414fdc37b01395
Vincent Pang <augostino@gmail.com>
2017-12-03 03:57:49 +0800

    update the input param array to readonly array

--- index.d.ts
+++ index.d.ts
@@ -9,9 +9,9 @@ declare class CanCan {
     public constructor(options?: CanCan.Option);
 
     public allow<T, U>(model: T,
-                       actions: string | string[],
-                       targets: U | U[] | string | string[],
-                       condition?: any | (<T, U, V>(performer: T, target: U, options?: V) => boolean)): void;
+                       actions: string | ReadonlyArray<string>,
+                       targets: U | ReadonlyArray<U> | string | ReadonlyArray<string>,
+                       condition?: object | (<T, U, V>(performer: T, target: U, options?: V) => boolean)): void;
 
     public can<T, U, V>(performer: T, action: string, target: U, options?: V): boolean;
 
